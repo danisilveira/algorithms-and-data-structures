6745bfe52b0f35b3ccb5bbe13af9b8400257dff0
Daniel Silveira <danilsbh1@gmail.com>
2022-11-08 22:19:33 -0300

    implementing the bubble sort

---
+++ algorithms/bubble_sort.go
@@ -0,0 +1,24 @@
+package algorithms
+
+import "golang.org/x/exp/constraints"
+
+func BubbleSort[T constraints.Ordered](items []T) {
+	sorted := false
+
+	for !sorted {
+		sorted = true
+		unsortedUntilIndex := len(items) - 1
+
+		for j := 0; j < unsortedUntilIndex; j++ {
+			if items[j] > items[j+1] {
+				temp := items[j]
+				items[j] = items[j+1]
+				items[j+1] = temp
+
+				sorted = false
+			}
+		}
+
+		unsortedUntilIndex--
+	}
+}
