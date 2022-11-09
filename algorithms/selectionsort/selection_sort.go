fc00b21e83c6a8b48b87e1e3d4f4166b4871bf19
Daniel Silveira <danilsbh1@gmail.com>
2022-11-08 23:31:55 -0300

    implementing the selection sort

---
+++ algorithms/selectionsort/selection_sort.go
@@ -0,0 +1,21 @@
+package selectionsort
+
+import "golang.org/x/exp/constraints"
+
+func Sort[T constraints.Ordered](items []T) {
+	for i := 0; i < len(items)-1; i++ {
+		lowest := i
+
+		for j := i + 1; j < len(items); j++ {
+			if items[j] < items[lowest] {
+				lowest = j
+			}
+		}
+
+		if lowest != i {
+			temp := items[i]
+			items[i] = items[lowest]
+			items[lowest] = temp
+		}
+	}
+}
---
+++ algorithms/selectionsort/selection_sort_test.go
@@ -0,0 +1,32 @@
+package selectionsort_test
+
+import (
+	"testing"
+
+	"github.com/danisilveira/algorithms-and-data-structures/algorithms/selectionsort"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSelectionSort_ShouldBeAbleToSortAnArrayOfNumbers(t *testing.T) {
+	items := []int{5, 4, 3, 2, 1}
+	selectionsort.Sort(items)
+	assert.Equal(t, []int{1, 2, 3, 4, 5}, items)
+}
+
+func TestSelectionSort_ShouldBeAbleToSortAnArrayAlreadySorted(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5}
+	selectionsort.Sort(items)
+	assert.Equal(t, []int{1, 2, 3, 4, 5}, items)
+}
+
+func TestSelectionSort_ShouldBeAbleToSortAnArrayOfFloats(t *testing.T) {
+	items := []float64{5.5, 4.4, 3.3, 2.2, 1.1}
+	selectionsort.Sort(items)
+	assert.Equal(t, []float64{1.1, 2.2, 3.3, 4.4, 5.5}, items)
+}
+
+func TestSelectionSort_ShouldBeAbleToSortAnArrayOfStrings(t *testing.T) {
+	items := []string{"e", "d", "c", "b", "a"}
+	selectionsort.Sort(items)
+	assert.Equal(t, []string{"a", "b", "c", "d", "e"}, items)
+}
