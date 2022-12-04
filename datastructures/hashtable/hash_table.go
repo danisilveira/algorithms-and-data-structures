674026e6b53541e77a987305c2d69e86d26e1985
Daniel Silveira <danilsbh1@gmail.com>
2022-12-04 18:56:47 -0300

    little refactor

--- datastructures/hashtable/hash_table.go
+++ datastructures/hashtable/hash_table.go
@@ -2,11 +2,14 @@ package hashtable
 
 import (
 	"hash/fnv"
+	"sync"
 )
 
 type Key interface {
 	~string
 }
+
+type HashGeneratorFunc[K Key] func(key K) int64
 
 type item[K Key, V any] struct {
 	key   K
@@ -15,16 +18,18 @@ type item[K Key, V any] struct {
 }
 
 type HashTable[K Key, V any] struct {
-	items             []*item[K, V]
-	capacity          int64
-	hashGeneratorFunc HashGeneratorFunc[K]
+	items         []*item[K, V]
+	capacity      int64
+	hashGenerator HashGeneratorFunc[K]
+	mu            *sync.RWMutex
 }
 
 func New[K Key, V any](capacity int64, options ...Option[K, V]) *HashTable[K, V] {
 	hashTable := HashTable[K, V]{
-		items:             make([]*item[K, V], capacity, capacity),
-		capacity:          capacity,
-		hashGeneratorFunc: defaultHashGeneratorFunc[K],
+		items:         make([]*item[K, V], capacity, capacity),
+		capacity:      capacity,
+		hashGenerator: defaultHashGeneratorFunc[K],
+		mu:            &sync.RWMutex{},
 	}
 
 	for _, option := range options {
@@ -34,10 +39,13 @@ func New[K Key, V any](capacity int64, options ...Option[K, V]) *HashTable[K, V]
 	return &hashTable
 }
 
-func (hashTable *HashTable[K, V]) Get(key K) (V, bool) {
-	hash := hashTable.hashGeneratorFunc(key) % hashTable.capacity
+func (h *HashTable[K, V]) Get(key K) (V, bool) {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
 
-	item := hashTable.items[hash]
+	hash := h.hashGenerator(key) % h.capacity
+
+	item := h.items[hash]
 	if item != nil && item.key == key {
 		return item.value, true
 	}
@@ -54,17 +62,20 @@ func (hashTable *HashTable[K, V]) Get(key K) (V, bool) {
 	return defaultValue, false
 }
 
-func (hashTable *HashTable[K, V]) Set(key K, value V) {
-	hash := hashTable.hashGeneratorFunc(key) % hashTable.capacity
+func (h *HashTable[K, V]) Set(key K, value V) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	hash := h.hashGenerator(key) % h.capacity
 
 	newItem := &item[K, V]{
 		key:   key,
 		value: value,
 	}
 
-	element := hashTable.items[hash]
+	element := h.items[hash]
 	if element == nil {
-		hashTable.items[hash] = newItem
+		h.items[hash] = newItem
 		return
 	}
 
@@ -85,24 +96,8 @@ func (hashTable *HashTable[K, V]) Set(key K, value V) {
 	element.next = newItem
 }
 
-type HashGeneratorFunc[K Key] func(key K) int64
-
 func defaultHashGeneratorFunc[K Key](key K) int64 {
 	hash := fnv.New32a()
 	hash.Write([]byte(key))
 	return int64(hash.Sum32())
-}
-
-type Option[K Key, V any] func(hashTable *HashTable[K, V])
-
-func WithDefaultHashGeneratorFunc[K Key, V any]() Option[K, V] {
-	return func(hashTable *HashTable[K, V]) {
-		hashTable.hashGeneratorFunc = defaultHashGeneratorFunc[K]
-	}
-}
-
-func WithCustomHashGeneratorFunc[K Key, V any](hashGeneratorFunc HashGeneratorFunc[K]) Option[K, V] {
-	return func(hashTable *HashTable[K, V]) {
-		hashTable.hashGeneratorFunc = hashGeneratorFunc
-	}
 }
