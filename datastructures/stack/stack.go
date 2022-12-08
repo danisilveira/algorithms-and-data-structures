eb23930212d8039e10155d83dd1a63f7e9aa1c12
Daniel Silveira <danilsbh1@gmail.com>
2022-12-08 18:14:25 -0300

    creating the stack interface

--- datastructures/stack/stack.go
+++ datastructures/stack/stack.go
@@ -7,13 +7,23 @@ var (
 	ErrStackIsEmpty  = errors.New("stack is empty")
 )
 
+type Stack[T any] interface {
+	Push(T) error
+	Pop() (T, error)
+	Top() T
+	Length() int
+	Capacity() int
+	Full() bool
+	Empty() bool
+}
+
 type stack[T any] struct {
 	items    []T
 	top      int
 	capacity int
 }
 
-func New[T any](capacity int) *stack[T] {
+func New[T any](capacity int) Stack[T] {
 	return &stack[T]{
 		items:    make([]T, capacity),
 		top:      0,
@@ -44,18 +54,22 @@ func (s *stack[T]) Pop() (T, error) {
 	return item, nil
 }
 
-func (s *stack[T]) Capacity() int {
-	return s.capacity
+func (s *stack[T]) Top() T {
+	return s.items[s.top]
 }
 
 func (s *stack[T]) Length() int {
 	return s.top
 }
 
-func (s *stack[T]) Empty() bool {
-	return s.top == 0
+func (s *stack[T]) Capacity() int {
+	return s.capacity
 }
 
 func (s *stack[T]) Full() bool {
 	return s.top == s.capacity
+}
+
+func (s *stack[T]) Empty() bool {
+	return s.top == 0
 }
