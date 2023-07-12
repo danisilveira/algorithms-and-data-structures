fe8c12aa7ca60bc046fc9407fefd02de0db24025
Daniel Silveira <danilsbh1@gmail.com>
2023-07-11 23:07:07 -0300

    a little refactor in bubble sort algorithm

--- algorithms/sorting/bubblesort/bubble_sort.go
+++ algorithms/sorting/bubblesort/bubble_sort.go
@@ -5,16 +5,13 @@ import "golang.org/x/exp/constraints"
 func Sort[T constraints.Ordered](items []T) {
 	sorted := false
 
+	unsortedUntilIndex := len(items) - 1
 	for !sorted {
 		sorted = true
-		unsortedUntilIndex := len(items) - 1
 
 		for j := 0; j < unsortedUntilIndex; j++ {
 			if items[j] > items[j+1] {
-				temp := items[j]
-				items[j] = items[j+1]
-				items[j+1] = temp
-
+				items[j], items[j+1] = items[j+1], items[j]
 				sorted = false
 			}
 		}
